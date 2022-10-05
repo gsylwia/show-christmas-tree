ed86fe25358b627e36fede131bef5aa3368b9ba7
Sylwia <g.sylwia@vp.pl>
2022-10-05 16:20:46 +0200

    finish christmas tree

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 let str = "";
-let treeHeight = 8;
+let treeHeight = 4;
 
 const showChristmasTree = (treeHeight) => {
-  for (let i = 0; i <= treeHeight ; i++) {
+  if (treeHeight === 1 && treeHeight > 10) {
+		return ""	
+	} 
+  
+  let x = ""
+  for (let i = 0; i <= treeHeight; i++) {
   
     for (let j = i; j <= treeHeight; j++) {
-     str += " "
+      str += " " 
+      x += " "
+      
+      if (i === 0 && j === (treeHeight -1)) {
+        str += "*"
+        x += " |" 
+      }
     }
 
-    for( let k = 0; k < (2*i)-1; k++) {
-     str+= "x"
+    for (let k = 0; k < (2*i)-1; k++) {
+      if (k === 0) {
+        str += "/"
+      }
+    
+      str+= "x"
     }
     
-    str += "\n"
-	}
-   console.log(str)
+    if (i === 0) {
+      str += "\n"
+    } else {
+      str += "\\ \n";
+    }  
+  }
+  
+  str += x
+  console.log(str)
+
 };
 
 showChristmasTree(treeHeight)
